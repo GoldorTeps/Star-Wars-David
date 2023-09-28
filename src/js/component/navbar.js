b8c4ce335542371defb7af3da422f0fe2e05ed7c
GoldorTeps <123986861+GoldorTeps@users.noreply.github.com>
2023-09-28 22:12:00 +0000

    navbar listo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,52 +1,79 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from '../store/appContext.js';
-import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	const { actions, store } = useContext(Context);
+  const { actions, store } = useContext(Context);
 
-	useEffect(() => {
-		actions.getDataPeople();
-		actions.getDataPlanets();
-		actions.getDataVehicles();
-	}, []);
+  useEffect(() => {
+    actions.getDataPeople();
+    actions.getDataPlanets();
+    actions.getDataVehicles();
+  }, []);
 
+  const handleDelete = (index) => {
+    actions.deleteFavorite(index);
+  };
 
-	const handleDelete = (index) => {
-		actions.deleteFavorite(index);
-	};
+  const renderFavorites = () => {
+    return (
+      <ul className="dropdown-menu">
+        {store.favorites.map((favorite, index) => (
+          <li key={index}>
+            <h2>
+              {favorite}
+              <button
+                className="btn btn-link text-danger"
+                onClick={() => handleDelete(index)}
+              >
+                <i className="bi bi-trash" title="Eliminar favorito"></i>
+              </button>
+            </h2>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
-	return (
-		<nav className="navbar fixed-top navbar-light bg-light bg-transparent mb-n4 ms-4 mt-4">
-			<Link to="/">
-				<img src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" width="200" height="100" alt="logo" />
-			</Link>
-				<>
-					<div className="ml-auto">
-						<div className="dropstart">
-							<button className="btn favorites dropdown-toggle my-5" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-								Favorites {store.favorites.length}
-							</button>
-							<ul className="dropdown-menu mx-auto">
-								{
-									store.favorites.map((favorite, index) => {
-										return (
-											<li style={{ color: "rgb(255, 179, 0)" }} key={index}>
-												<h2>{favorite}
-													<box-icon type='solid' class='bx-lg bx-tada-hover' color="orange" name='trash-alt' onClick={() => handleDelete(index)}></box-icon>
-													&nbsp;
-													<button style={{ fontSize: "24px", color: "rgb(221, 85, 51)", background: "none", border: "none" }} onClick={() => handleDelete(index)}>&#10006;</button>
-												</h2>
-											</li>
-										)
-									})
-								}
-							</ul>
-							
-						</div>	
-					</div>
-				</>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Favorites {store.favorites.length}
+              </a>
+              {renderFavorites()}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
